3e70c00b5f3460eea1146f1d3440ed85da82dc79
Alexander Aspinall <alex.aspinall@blueyonder.co.uk>
2024-12-20 12:03:03 +0000

    Add excerpt field to Post model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 # Create your models here.
@@ -7,11 +8,19 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 class Post(models.Model):
-    title = title = models.CharField(max_length=200, unique=True)
+    title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts")
     content = models.TextField()
+    excerpt = models.CharField(max_length=400, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
+# class Cheese (models.Model):
+#     name = models.CharField(max_length=200, unique=True)
+#     stink = models.IntegerField(
+#         validators=[
+#             MaxValueValidator(100),
+#             MinValueValidator(1)
+#         ])

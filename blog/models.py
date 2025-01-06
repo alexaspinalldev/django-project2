26ce7c94fb883bd044bfd5e3598cb3ba16e2620f
Alexander Aspinall <alex.aspinall@blueyonder.co.uk>
2025-01-06 14:04:50 +0000

    Match files to CI repo and restore endblock to base.html

--- blog/models.py
+++ blog/models.py
@@ -1,37 +1,41 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
 
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 
-STATUS = ((0, "Draft"), (1, "Published"))
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     content = models.TextField()
-    excerpt = models.CharField(max_length=400, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    is_hate_speech = models.CharField(null=True)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
     class Meta:
-        ordering = ["-created_on", "author"]
+        ordering = ["-created_on"]
+
     def __str__(self):
-        return f'{self.title} - Author: {self.author}'
+        return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_comments")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
-    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-    edgy = models.BooleanField()
+    approved = models.BooleanField(default=False)
+
     class Meta:
         ordering = ["created_on"]
+
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
